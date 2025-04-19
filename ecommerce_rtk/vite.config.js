9a6274ee160bc85085fa88be79009683bab4113e
hassan2-aamir <haamir.bscs23seecs@seecs.edu.pk>
2025-04-20 00:29:23 +0300

    setup github pages

--- ecommerce_rtk/vite.config.js
+++ ecommerce_rtk/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/E-Commerce-Data-Rendering-using-React-Toolkit/', // Set base path to your repo name
 })
